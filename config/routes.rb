042980e0b9ec9e26d1c4221a5da0207250805d5f
marcelabrenes <marce89455@gmail.com>
2021-03-29 12:35:06 -0600

    Added Order and Users models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admins
-  root to: 'home#index'
+  #root to: 'home#index'
+  
+  authenticate :admin do 
+    resources :products
+    resources :categories
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
