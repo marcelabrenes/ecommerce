4e828d95643152401a22f5d6a8595a2bef87a916
marcelabrenes <marce89455@gmail.com>
2021-03-31 15:38:56 -0600

    Some chages made

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  resources :carts, only: %i[show update]
   devise_for :users
   devise_for :admins
-  #root to: 'home#index'
+  root to: 'home#index'
   
   authenticate :admin do 
     resources :products
     resources :categories
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
